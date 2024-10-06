c4243fcf1cfa634a91f7a3f1bd2566fd31c1fecf
amishmishra27 <amis0014@student.monash.edu>
2024-10-06 16:57:10 +1100

    Add files via upload

---
+++ event_vis.js
@@ -0,0 +1,4 @@
+var spec2 = "event-bubble-plot.vg.json";
+vegaEmbed('#bar_chart', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
